98a99f214b1bb7f411b3a608732f5c7e493f7a7e
kapil4611 <kapil14516@gmail.com>
2025-04-28 13:51:59 +0530

    jwt integrated and .env-example added

---
+++ jwt.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+
+const jwtAuthMiddleware = (req, res, next) => {
+    // first check request headers has authentication or not
+    const authorization = req.headers.authorization;
+    if(!authorization) return res.json({error: "Token not found"});
+
+    // Extract the jwt token from request headers 
+    // Normaly token passes in (Authorization/authorization)request header
+    // const token = req.headers.authorization;
+
+    // Bearer token passed in request header
+    const token = req.headers.authorization.split(' ')[1];
+
+    if(!token) return res.status(401).json({error: "Unauthorized"});
+    
+    try{
+        // verify token
+        const decoded = jwt.verify(token, (process.env.JWT_SECRET || "jwtsecretkey"));
+
+        // Attach user information to the request object
+        req.user = decoded;
+        next();
+    } catch(err){
+        console.log(err);
+        res.status(401).json({error: "Invalid token"});
+    }
+}
+
+// Function to generate JWT token
+const generateToken = (userData) => {
+    // Generate new JWT token using user data
+    // UserData should be proper object then only expire token featue will work
+    return jwt.sign(userData, (process.env.JWT_SECRET || "jwtsecretkey"), {expiresIn: 30000});
+};
+
+module.exports = {jwtAuthMiddleware, generateToken};
--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,24 +1,76 @@
 const express = require("express");
 const router = express.Router();
 const Person = require("../models/Person");
-const { error } = require("console");
+// const { error } = require("console");
+const {jwtAuthMiddleware, generateToken} = require("../jwt");
 
 const passport = require("../auth");
 const localAuthMiddleware = passport.authenticate("local", {session: false});
 
-router.post("/", async (req, res) => {
+router.post("/signup", async (req, res) => {
     try {
         const data = req.body;
         const newPerson = Person(data);
         const response = await newPerson.save();
-        res.status(200).json(response);
+        console.log("Data saved");
+
+        const payload = {
+            id: response.id,
+            username: response.username,
+        };
+        console.log(payload);
+
+        const token = generateToken(payload);
+        console.log("Token is: ",token);
+
+        res.status(200).json({response: response, token: token});
     } catch(e) {
         res.status(500).json({error: "Internal server error"});
     }
     
 });
 
-router.get("/", localAuthMiddleware, async(req, res) => {
+router.post("/login", async(req, res) => {
+    try{
+        // Extract username and password from request body
+        const {username, password} = req.body;
+        const user = await Person.findOne({username: username});
+        if(!user || !(await user.comparePassword(password))) {
+            return res.status(401).json({error: "Invalid usename or password"});
+        } 
+        // generate token
+        const payload = {
+            id: user.id,
+            username: user.username,
+        };
+        const token = generateToken(payload);
+        // return token as response
+        res.json({token});
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({error: "Invalid server error"})
+    }
+});
+
+// Profile route
+router.get("/profile", jwtAuthMiddleware, async (req, res) => {
+    try {
+        const userData = req.user;
+        const userId = userData.id;
+        const user = await Person.findById(userId);
+        res.status(200).json(user);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({error: "Internal server error"});
+    }
+});
+
+// use localAuthMiddleware middleware in below request to implement localauthentication
+// use jwtAuthMiddleware middleware in below request to implement jwtauthentication
+// jwtauthentication(we can access any protected route without any username and password)
+
+// GET method to get all person
+router.get("/", jwtAuthMiddleware, async(req, res) => {
     try {
         const data = await Person.find();
         console.log("Data fatched");
@@ -46,6 +98,7 @@ router.get("/:workType", localAuthMiddleware, async(req, res) => {
     }
 });
 
+// update by passing id
 router.put("/:id", async(req, res) => {
     try{
         const personId = req.params.id
